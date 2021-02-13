5c131701d2547db48d81964f7523ee1cbf83ecf2
luccavp12 <lucca12vp@gmail.com>
2021-02-12 22:42:49 -0800

    Add queueingFunction that handles uniform cost search

--- uniformcostsearch.py
+++ uniformcostsearch.py
@@ -20,9 +20,12 @@ D = [[1, 0, 3],
     [4, 2, 6],
     [7, 5, 8]]
 
+E = [[1, 2, 3], 
+    [4, 5, 6],
+    [7, 0, 8]]
 
-queue.append(A)
-queue.append(B)
+# queue.append(A)
+# queue.append(B)
 
 # for i in range(len(queue)): #for every board in the queue
 #     for j in range(3): #for every row in board
@@ -71,7 +74,9 @@ class Problem: # used in 'main' when creating an initial problem
     def __init__(self, initialState):
         self.initialState = initialState
         self.operators = [[-1,0], [1, 0], [0,-1], [0,1]]
-        self.goalState =   [[1, 2, 3],[4, 5, 6],[7, 8, 0]]
+        self.goalState = [[1, 2, 3], 
+                         [4, 5, 6],
+                         [7, 8, 0]]
 
     def printBoard(self):
         for j in range(3): # for every row in board
@@ -93,7 +98,7 @@ class Node:
         self.dimension = len(initialState[0])
         # print("self.dimension: " + str(self.dimension))
         self.depth = 0      # can increment every time another one is created
-        self.goalDist = 0   # used in the heuristic
+        self.heuristic = 0   # used in the heuristic
     def printBoard(self):
         for j in range(3): # for every row in board
             print(str(self.state[j][0]) + ' ' + str(self.state[j][1]) + ' ' + str(self.state[j][2]))
@@ -106,8 +111,14 @@ class NodeQueue:
         self.queue.append(initialNode)
         # print("self.queue: " + str(self.queue[0]))
         
-    # def makeQueue(self):
-    #     return self.queue
+    def printBoard(self):
+        for i in self.queue:
+            for j in range(3): # for every row in board
+                print(str(i.state[j][0]) + ' ' + str(i.state[j][1]) + ' ' + str(i.state[j][2]))
+        
+    def concat(self, newNodes):
+        concatination = newNodes + self.queue
+        self.queue = concatination
 
 def findBlank(state):
     listNum = 0
@@ -146,7 +157,8 @@ def expand(node, operators):
 
         child.state[i[0]][i[1]] = 0                                 # Sets the switched possition to blank value (0) and sets 
         child.state[blankLocation[0]][blankLocation[1]] = tempVal   # Sets the original blank spot to temp value, indicating a switch has taken place
-
+        print("child:")
+        child.printBoard()
         children.append(child)                                      # Appends new child to child list
 
     return children
@@ -154,8 +166,50 @@ def expand(node, operators):
 def queueingFunction(flag, prevNodes, newNodes):
     # This function takes in all of the nodes in the current queue, and then fixes them to the correct order.
     # for instance, 
-    print("pushes the new nodes onto the queue")
-    return []
+    global duplicates
+    # print("duplicates:")
+    # if flag == 1:
+
+    for i in newNodes:
+        print("i: " + str(i.state))
+        if i.state in duplicates:
+            print("i.state is in duplicates!")
+            newNodes.remove(i)
+        else:
+            print("i.state is a new unique state and will be added to dups for later")
+            duplicates.append(i)
+
+    prevNodes.concat(newNodes)
+    print("prevNodes.printBoard():")
+    prevNodes.printBoard()
+    return prevNodes
+
+
+        # for j in duplicates:
+        #     if i.state == j.state:
+        #         print("found a dup in the children, removing now!")
+
+
+
+        # else:
+        #     duplicates.append(i)
+        #     print("duplicates length" + str(len(duplicates)))
+        #     if len(duplicates) == 1:
+        #         print("duplicates: " + str(duplicates[0].state))
+                # print("duplicates: " + str(duplicates[1].state))
+                # print("duplicates: " + str(duplicates[2].state))
+                # print("duplicates: " + str(duplicates[3].state))
+                # print("duplicates: " + str(duplicates[4].state))
+                
+                
+
+
+    # duplicates = duplicates + prevNodes.queue
+
+    # elif flag == 2:
+    #     print("TODO: misplaced tile function")
+    # elif flag == 3:
+    #     print("TODO: manhattan distance function")
     
 
 def generalSearch(Problem, queueingFunctionFlag):
@@ -163,24 +217,30 @@ def generalSearch(Problem, queueingFunctionFlag):
     print("rootNode: " + str(rootNode.state))
 
     nodes = NodeQueue(rootNode)
-    print("Queue: " + str(nodes.queue[0].state))
+    # print("Queue: " + str(nodes.queue[0].state))
 
     # childrenQueue = expand(rootNode)
 
-    print(len(nodes.queue))
+    # print(len(nodes.queue))
+    # for x in range(5):
     while nodes:                                                                # While the queue of nodes is not empty
         currNode = nodes.queue.pop()                                            # Gets the top node off of the queue
-        if Problem.goalTest(currNode):
+        print("currNode:")
+        currNode.printBoard()
+        if Problem.goalTest(currNode.state):
+            print("Found the goal state!")
             return currNode
         nodes = queueingFunction(queueingFunctionFlag, nodes, expand(currNode, Problem.operators))    # need to add problem.operators but still confused
+        # print("queueingFunction called")
     return False
 
 
-
-Problem = Problem(D)
+Problem = Problem(C)
+duplicates = [Problem.initialState]
 # endNode = generalSearch(Problem, "uniform")
-Result = generalSearch(Problem, "uniform")
+Result = generalSearch(Problem, 1)
 if Result == False:
     print("Failed to find a solution")
 else:
     Result.printBoard()
+    print("Result.depth: " + str(Result.depth))
